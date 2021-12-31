27639f9761c20dd44bc97a1825cf3ccb8b478803
shAtud <91322794+shAtud@users.noreply.github.com>
2021-12-31 21:11:05 +0100

    Update Square.js

--- build a tic tac toe game/Square.js
+++ build a tic tac toe game/Square.js
@@ -1,13 +1,12 @@
-const Square = ({label,i,j,setGridTovalue})=>{
-    
-  
+const Square = ({label,i,j,setValue})=>{
     return(
-<div 
-    className="bg-gray-700 w-[100px] h-[100px] border-2 border-black  text-[70px] relative text-white hover:cursor-pointer hover:scale-[105%] rounded-[5px]"
-    onClick={()=>setGridTovalue(i,j)}
->
-        <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] ">{label}</div>
- </div>
+        <div 
+            className="bg-gray-700 border-2 border-black w-[100px] h-[100px] relative hover:cursor-pointer hover:scale-[105%] rounded-[5px] " 
+            onClick={()=>setValue(i,j)}
+            >
+            <div className="absolute text-white text-[70px] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] ">{label}</div>
+        </div>
     )
 }
+
 export default Square;
