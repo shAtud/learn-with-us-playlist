4b17bcd813382d9b3d6e247755a4e138cc7d2dd7
shAtud <91322794+shAtud@users.noreply.github.com>
2021-12-31 21:10:45 +0100

    Update Game.js

--- build a tic tac toe game/Game.js
+++ build a tic tac toe game/Game.js
@@ -1,141 +1,151 @@
-import { useState } from "react";
-import Square from "./Square";
+import { useState } from "react"
+import Square from "./Square"
 const INITIAL_GRID = [
     ['-','-','-'],
     ['-','-','-'],
-    ['-','-','-']
-];
+    ['-','-','-'],
+]
+
 const Game = props=>{
-    const [grid,setGrid] = useState(INITIAL_GRID)
+    const [grid,setGrid] = useState(INITIAL_GRID);
+    const [turn,setTurn] = useState(Math.random()>0.5?false:true);
     const [gameEnd,setGameEnd] = useState(false);
-    const [gameWinner,setGameWinner] = useState(null)
+    const [gameWinner,setGameWinner] = useState(null);
+
+    /*
+            turn = false => player1 (O)
+            turn = true  => player2 (X)
+    */
     const checkLignes = (newGrid)=>{
-        let winner = null;
-        for(let i=0;i<3;i++){
-            let x,o = 0;
-            for(let j=0;j<3;j++){
-                if(newGrid[i][j] === 'X' ) x++;
-                if(newGrid[i][j] === 'O' ) o++;
-            }
-            if(x ===3) {
-                winner = 'X';
-                break;
-
-            }
-            else if(o ===3) {
-                winner = 'O';
-                break;
-
-            }
-           
-        }
-        return winner;
-
-    }
-    const checkCulomns = (newGrid)=>{
-        let winner = null;
-        for(let i=0;i<3;i++){
-            let x,o = 0;
-            for(let j=0;j<3;j++){
-                if(newGrid[j][i] === 'X' ) x++;
-                if(newGrid[j][i] === 'O' ) o++;
-            }
-            if(x ===3) {
-                winner = 'X';
-                break;
-
-            }
-            else if(o ===3) {
-                winner = 'O';
-                break;
-
-            }
-           
-        }
-        return winner;
-
-    }
-    const checkDigonals = (newGrid)=>{
-        let x,o = 0;
-        let winner = null;
-        for(let i=0;i<3;i++){
-            if(newGrid[i][i]==='X') x++;
-            if(newGrid[i][i]==='O') o++;
-        }
-        if( x===3) winner = 'X'
-        else if (o === 3) winner = 'O';
-        if(winner === null){
-            for(let i=0;i<3;i++){
-                if(newGrid[i][3-i]==='X') x++;
-                if(newGrid[i][3-i]==='O') o++;
-            }
-            if( x===3) winner = 'X'
-            else if (o === 3) winner = 'O';
-        }
-
-        return winner;
-    }
-
-    const setGridTovalue = (i,j)=>{
-        if(gameEnd) return;
-        const newGrid = [...grid];
-        if(grid[i][j]!='-') return;
-        newGrid[i][j] = !turn?'X':'O';
-        setGrid(newGrid)
-        
-        setTurn(!turn)
-      
-        let winner = checkLignes(newGrid);
-        if(winner===null) winner = checkCulomns(newGrid);
-        if(winner===null) winner = checkDigonals(newGrid);
-
-        if(winner){
-            setGameEnd(true)
-           
-            setGrid(INITIAL_GRID)
-            setGameWinner(winner)
-            console.log(winner)
+                
+                for(let i=0;i<3;i++){
+                    let o = 0;
+                    let x = 0;
+                    for(let j=0;j<3;j++){
+                        if(newGrid[i][j] === 'X') x++
+                        if(newGrid[i][j] === 'O') o++;
+                    }
+                   
+                    if(x === 3){
+                       return 'X';
+                    }
+                    else if( o === 3){
+                        return 'O';
+                    }
+                   
+                }
                
+                return null;
+         }
+    const checkCulumns = (newGrid)=>{
+                
+                for(let i=0;i<3;i++){
+                    let x = 0;
+                    let o = 0;
+                    for(let j=0;j<3;j++){
+                        if(newGrid[j][i] === 'X') x++
+                        if(newGrid[j][i] === 'O') o++;
+                    }
+                    if(x === 3){
+                       return 'X';
+                    }
+                    else if( o === 3){
+                        return 'O';
+                    }
+                }
+                return null;
+            }
+    const checkDiagonals = (newGrid)=>{
+                //first diagonal
+                let x = 0;
+                let o = 0;
+                for(let i=0;i<3;i++){
+                    if(newGrid[i][i] === 'X') x++
+                    if( newGrid[i][i] === 'O') o++;
+                }
+                if( x === 3 ) return 'X'
+                else if( o === 3 ) return 'O';
+                //second diagonal
+                x = 0;
+                o = 0;
+                for(let i=0;i<3;i++){
+                    if(newGrid[i][2-i] === 'X') x++
+                    if( newGrid[i][2-i] === 'O') o++;
+                }
+                if( x === 3 ) return 'X'
+                else if( o === 3 ) return 'O';
+        
+                return null;
+            }
+    const setValue = (i,j) =>{
+        
+        const newGrid = [...grid];
+        if(newGrid[i][j]!='-' ) return;
+        newGrid[i][j] = !turn?'O':'X';
+        setGrid(newGrid)
+        setTurn(!turn)
+        /*
+         *****************who is the winner ?*****************
+        */
+        let winner = null;
+        winner = checkLignes(newGrid);
+        console.log(checkLignes(newGrid),checkCulumns(newGrid),checkDiagonals(newGrid))
+        if(winner === null) winner = checkCulumns(newGrid);
+        if(winner === null) winner = checkDiagonals(newGrid);
+        
+        if(winner){
+            setGameEnd(true);
+            setGameWinner(winner)
+
+          
+         
+         
+       
+          
+         
+           
+           
+            
+           
+
         }
 
-        
-    }
-    const [turn,setTurn] = useState(Math.random()>0.5?false:true)
-    // false : player1 (x)
-    // true :  player2 (o)
 
+    }
+   
     return(
         <>
         {
             gameEnd?(
-                <div>{gameWinner==='X'?'player1(X) won the game':'player2(O) won the game'}</div>
+                <div>
+                    the {gameWinner==='O'?'player2 (O)':'player1 (x)'} won the game
+                </div>
             ):(
-                <div>{!turn?'player1:(x)':'player2:(o)'}</div>
+                <div>{!turn?"player1 (O) :":"player2 (X)"}</div>
             )
         }
        
-        <div className="bg-gray-500 w-[300px] h-[300px] flex flex-wrap ">
-               
-                {
-                    grid.map(((gLine,i)=>{
-                        return(
-                            gLine.map((el,j)=>{
-                                return(
-                                    <Square 
-                                        label={el}
-                                        i={i}
-                                        j={j}
-                                        setGridTovalue={setGridTovalue}
-                                        
-                                    />
-                               )
-                            })
-                        )
-                       
-                    }))
-                }
+        <div className="w-[300px] h-[300px] bg-gray-500 flex flex-wrap">
+            
+            {
+                grid.map((gLine,i)=>{
+                    return(
+                        gLine.map((el,j)=>{
+                           return(
+                            <Square
+                                label = {el}
+                                i={i}
+                                j={j}
+                                setValue={setValue}
+                            />
+                           )
+                        })
+                    )
+                })
+            }
         </div>
-        </>
+    </>
     )
 }
+
 export default Game;
