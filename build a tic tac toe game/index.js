c187397225bf1f064c6794a11d0794035afad530
shAtud <91322794+shAtud@users.noreply.github.com>
2021-12-31 21:12:31 +0100

    Update index.js

--- build a tic tac toe game/index.js
+++ build a tic tac toe game/index.js
@@ -3,14 +3,15 @@ import Game from "../components/Game";
 
 
 
+
 export default function Home() {  
  
   return (
-    <div className="flex flex-col  font-[600] justify-center items-center h-screen text-2xl ">
-      <Game/>
+    <div className="flex flex-col  font-[600] justify-center items-center h-screen text-2xl">
+    
       
-   
-      
+        <Game/>
+     
         
          
 
