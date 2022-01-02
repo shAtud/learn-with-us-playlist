ed54440dc7e292b94c1657d7f07d760c113222e7
shAtud <91322794+shAtud@users.noreply.github.com>
2022-01-02 13:55:53 +0100

    Create index.js

---
+++ todo list react | next | tailwind css/index.js
@@ -0,0 +1,19 @@
+import TodoWindow from "../components/TodoWindow";
+
+
+
+
+
+export default function Home() {  
+ 
+  return (
+    <div className="flex flex-col  font-[600] justify-center items-center h-screen text-2xl">
+       <TodoWindow/>
+        
+     
+        
+         
+
+    </div>
+  )
+}
